e2fae5c674037805df3a109651330270480e385a
Samir Hussain <shussain@credil.org>
2016-03-26 17:50:25 -0400

    Adding more comments for configuring tasks_insert script

--- task_insert/tasks_insert.py
+++ task_insert/tasks_insert.py
@@ -2,18 +2,20 @@
 #
 # Modified from Google sample site
 
-""" Command-line application for inserting tasks.
+""" Command-line application for insert defautl tasks into a task list.
+    For the use case, where you have the same tasks for week day (log hours,
+    check mail, etc) and slightly different tasks for weekends.
 
 Usage:
   $ python tasks,py
 
-INSTALLATION
+= INSTALLATION =
+
 Get all dependencies
     sudo apt-get install python-dev build-essential
 
 Install dependencies
     sudo pip install --upgrade google-api-python-client
-
 
 cat runtasks_insert.sh
 
@@ -30,6 +32,12 @@ You will need to generate a clients_secrets.json file. It can be acquired by goi
     Select "Download JSON"
     Copy and save to the root of this file and make sure its named clients_secrets.json
 
+= CONFIGURATION =
+
+* modify TASKS_LISTS_TO_FOLLOW to the name of the task list that you
+  want to insert tasks into
+* modify WEEKDAY_TASK to list tasks that you wanted listed for weekdays
+* modify WEEKEND_TASK for your weekend tasks.
 
 """
 
@@ -57,6 +65,7 @@ parser = argparse.ArgumentParser(
 CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
 
 TASKS_LISTS_TO_FOLLOW = 'todo_d_daily'
+
 WEEKDAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 WEEKENDS = ["Saturday/Sunday"]
 

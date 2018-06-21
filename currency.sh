cadeb5bb2ee540cbda7a493e8e7817641c2cbb62
Samir Hussain <shussain@xelerance.com>
2018-06-20 22:47:22 -0400

    Move aorund constants in currency.sh

--- currency.sh
+++ currency.sh
@@ -6,12 +6,12 @@
 # most currency conversion sites is far from ideal
 
 
-getCurrency () {
-    GOOGLE_FINANCE="https://www.google.com/search?q=$1+to+$2"
-    AGENT="Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3"
-    BEGINING="s/<!doctype.*\J7UKTe\">//g"
-    ENDING='s/<\/div.*//'
+GOOGLE_FINANCE="https://www.google.com/search?q=$1+to+$2"
+AGENT="Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3"
+BEGINING="s/<!doctype.*\J7UKTe\">//g"
+ENDING='s/<\/div.*//'
 
+getCurrency () {
     wget "$GOOGLE_FINANCE" -U $AGENT -o /dev/null -O /dev/stdout | sed -e $BEGINING | sed -e $ENDING | head -n 1
 }
 

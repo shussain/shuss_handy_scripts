4cb1f38ebc42497dea072ba82242b714d7434705
Samir Hussain <shussain@xelerance.com>
2022-09-14 17:50:58 -0400

    Add tracking Australian and NZ dollar conversion rate

--- currency.sh
+++ currency.sh
@@ -2,7 +2,7 @@
 
 # Get various conversion rate from open exchange rates
 curl -X GET -s https://openexchangerates.org/api/latest.json\?app_id\=ID_STRING > /tmp/currency.txt
-egrep "CAD|PHP|INR|PKR|JPY|GBP|EUR|BTC"  /tmp/currency.txt
+egrep "CAD|PHP|INR|PKR|JPY|GBP|EUR|AUD|NZD|BTC"  /tmp/currency.txt
 
 CAD_CASH=`grep "CAD" /tmp/currency.txt  |sed  's/"CAD": //' | sed 's/,//' | sed 's/    //g'`
 USD_CAD=`echo 1/$CAD_CASH | bc -l`
